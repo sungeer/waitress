e2119047e24cba3b93c625f88ba6faf89b478bd4
wangxun <1013296326@qq.com>
2026-07-21 17:05:30 +0800

    fix

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -7,14 +7,14 @@ bind = '0.0.0.0:8848'
 chdir = '/srv/poetess'
 pidfile = '/srv/run/poetess.pid'
 
-workers = 2
+workers = 4
 worker_class = 'uvicorn.workers.UvicornWorker'
 
 keepalive = 5  # 保持客户端连接的时长
 timeout = 120  # worker 处理单个请求超时时间(秒)
 graceful_timeout = 30  # 平滑重启等待时长(秒)
-max_requests = 1000  # 防止内存泄漏，worker处理1000个请求后重启
-max_requests_jitter = 50  # 添加随机抖动，防止worker同时重启
+# max_requests = 1000  # 防止内存泄漏，worker处理1000个请求后重启
+# max_requests_jitter = 50  # 添加随机抖动，防止worker同时重启
 
 capture_output = True
 # accesslog = '/srv/logs/access.log'
