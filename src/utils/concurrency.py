ec360e7a2db0c3b58cbf16f6bf5c54140ef292ec
Want Sun <1013296326@qq.com>
2026-08-26 06:12:53 +0800

    fix

--- src/utils/concurrency.py
+++ src/utils/concurrency.py
@@ -10,7 +10,10 @@ def _run_with_context(context, func, *args):
 async def run_in_threadpool(executor, func, *args, **kwargs):
     # executor type is 'ThreadPoolExecutor'
     loop = asyncio.get_running_loop()  # 当前正在运行的事件循环实例
+
     if kwargs:
         func = functools.partial(func, **kwargs)
+
     context = contextvars.copy_context()  # 捕获调用方 context(含 trace_id)
+
     return await loop.run_in_executor(executor, _run_with_context, context, func, *args)  # type: ignore[misc]
