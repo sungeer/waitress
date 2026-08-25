222afa3f02f92c0aa66886891644935cd8472628
wangxun <1013296326@qq.com>
2026-08-25 11:05:17 +0800

    fix

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -10,11 +10,4 @@ pidfile = '/srv/run/waitress.pid'
 workers = 1
 worker_class = 'uvicorn.workers.UvicornWorker'
 
-keepalive = 5  # 保持客户端连接的时长
-timeout = 30  # worker 处理单个请求超时时间(秒)
-graceful_timeout = 30  # 平滑重启等待时长(秒)
-
-accesslog = None  # 关闭访问日志
-errorlog = '-'  # 改为 '-'，将 Gunicorn 自身错误输出到 stderr
-loglevel = 'info'
-capture_output = False  # 防止 Loguru 的日志被 Gunicorn 再抓取一次
+errorlog = '/srv/logs/gunicorn.log'
