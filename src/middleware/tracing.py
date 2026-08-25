1a4074b2700c0d93c2e53e8b5990e1ab7791e845
Want Sun <1013296326@qq.com>
2026-08-26 06:50:51 +0800

    fix

--- src/middleware/tracing.py
+++ src/middleware/tracing.py
@@ -11,9 +11,11 @@ class RequestIdMiddleware(BaseHTTPMiddleware):
     async def dispatch(self, request, call_next):
         trace_id = new_request_id()
 
+        request.state.trace_id = trace_id
+
+        start = time.perf_counter()
+
         with logger.contextualize(trace_id=trace_id):
-            request.state.trace_id = trace_id
-            start = time.perf_counter()
             logger.info('hit method={} path={}', request.method, request.url.path)
 
             status = 500  # 默认值：未处理异常统一按 500 收尾
@@ -29,5 +31,6 @@ class RequestIdMiddleware(BaseHTTPMiddleware):
                     status,
                     elapsed_ms,
                 )
-            response.headers['X-Request-ID'] = trace_id
-            return response
+
+        response.headers['X-Request-ID'] = trace_id
+        return response
