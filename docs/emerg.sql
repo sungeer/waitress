eb852690b505e7e8521ebb95d72ee32383bbca74
wangxun <1013296326@qq.com>
2026-09-16 08:34:57 +0800

    fix

--- docs/emerg.sql
+++ docs/emerg.sql
@@ -4,6 +4,12 @@
 -- 查看有哪些行号符合
 grep -n -e "- ERROR" waitress.log
 
+-- 同时满足
+grep -n -e "- ERROR" waitress.log | grep "0916"
+
+-- 任有其一
+grep -n -e "- ERROR" -e "0916" waitress.log
+
 
 -- 定位到指定行
 less +3 waitress.log
